ee86ffe43c9dd147513881ecea1640331cf02f34
Alexander_Kolyadin <psixsikse666@gmail.com>
2022-04-26 23:26:16 +0300

    add IfElse

---
+++ examp005_ConditionIfElse/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.Readline();
+
+if(username.ToLower() == "Маша") 
+{
+    Console.Writeline("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привeт, ");
+    Console.WriteLine(username);
+}
+
+
+
+    
